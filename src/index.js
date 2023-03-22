8970a3b21c18c2816725de15bdfe72568bdd0c55
Lucas Cercal <lucascercal.fontes@gmail.com>
2023-03-22 11:21:16 -0300

    Added checksCreateTodosUserAvailability

--- src/index.js
+++ src/index.js
@@ -21,7 +21,13 @@ function checksExistsUserAccount(request, response, next) {
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (user.pro === true || (user.pro === false && user.todos.length < 10)) {
+    return next();
+  } else {
+    return response.status(403).json({ error: "User not allowed to create more todos." });
+  }
 }
 
 function checksTodoExists(request, response, next) {
